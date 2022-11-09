3f4b4cfc4d79f962b4749a39671d3c8781cde3a8
Tim Ilyin <tilyin@gmail.com>
2022-11-09 12:23:09 +0300

    work in progress as of Ср 09 ноя 2022 12:23:09 +03

--- git-move_remote.sh
+++ git-move_remote.sh
@@ -5,6 +5,6 @@ WORK_ITEM=$1
 GIT_ITEM=$WORK_ITEM.git
 
 git clone --bare $WORK_ITEM $GIT_ITEM
-scp -r -i /home/tim/.ssh/id_ed25519 -P 4961 $GIT_ITEM git@10.1.1.254:/home/git/data/$GIT_ITEM
-ssh -p 4961 -i /home/tim/.ssh/id_ed25519 git@10.1.1.254 "cd /home/git/data/$GIT_ITEM; git init --bare --share"
-rm -r $GIT_ITEM
+scp -r -i $HOME/.ssh/id_ed25519 -P 4961 $GIT_ITEM master@95.165.133.121:/home/master/git/$GIT_ITEM
+ssh -p 4961 -i /home/tim/.ssh/id_ed25519 master@95.165.133.121 -t "cd /home/master/git/$GIT_ITEM; git init --bare --share"
+#rm -r $GIT_ITEM
